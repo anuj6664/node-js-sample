a2ae9aac82a663b0faecd349191547935d6a9a43
anuj6664 <anujkumar6664@gmail.com>
2026-03-12 19:34:50 +0530

    hello jenkins

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Jenkins World!')
+  response.send('Hello Jenkins')
 })
 
 app.listen(app.get('port'), function() {
